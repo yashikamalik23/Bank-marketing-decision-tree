b50a376c0638fac099a29e0963c56968578eea5b
yashikamalik23 <152992960+yashikamalik23@users.noreply.github.com>
2025-07-01 11:11:45 +0530

    Add files via upload
    This project implements a complete machine learning pipeline on the Bank Marketing dataset to predict whether a customer will subscribe to a term deposit. Using Python, pandas, and scikit-learn, it preprocesses data by encoding categorical variables, splits data into training and testing sets, and trains a Decision Tree Classifier with a controlled depth to reduce overfitting.
    
    The model’s performance is evaluated through accuracy, classification reports, and confusion matrices, providing clear insights into prediction effectiveness. Additionally, the trained decision tree is visualized to show the most important features influencing customer decisions. This project demonstrates a practical example of supervised classification in banking marketing analysis, making it a valuable resource for beginners and practitioners interested in data preprocessing, model evaluation, and interpretability.

---
+++ task3.py
@@ -0,0 +1,40 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Yashika malik\AppData\Local\Programs\Python\Python312\task3.py
+
+= RESTART: C:\Users\Yashika malik\AppData\Local\Programs\Python\Python312\task3.py
+Accuracy: 0.8051948051948052
+
+Classification Report:
+               precision    recall  f1-score   support
+
+           0       0.86      0.74      0.80      1166
+           1       0.76      0.87      0.81      1067
+
+    accuracy                           0.81      2233
+   macro avg       0.81      0.81      0.81      2233
+weighted avg       0.81      0.81      0.80      2233
+
+
+Confusion Matrix:
+ [[867 299]
+ [136 931]]
+>>> 
+========================= RESTART: C:\Users\Yashika malik\AppData\Local\Programs\Python\Python312\task3.py ========================
+Accuracy: 0.8051948051948052
+
+Classification Report:
+               precision    recall  f1-score   support
+
+           0       0.86      0.74      0.80      1166
+           1       0.76      0.87      0.81      1067
+
+    accuracy                           0.81      2233
+   macro avg       0.81      0.81      0.81      2233
+weighted avg       0.81      0.81      0.80      2233
+
+
+Confusion Matrix:
+ [[867 299]
+ [136 931]]
